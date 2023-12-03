0f0d37e367dac6b16e522056cfaebcc440e3fe71
Devjyoti Mukherjee <devjyotidgr2023@gmail.com>
2023-12-03 10:59:39 +0530

    Number is a prime number or not.

--- primeNumber.js
+++ primeNumber.js
@@ -1,1 +1,24 @@
-console.log('Hello world');
+function primeNumber(num) {
+  let isPrime = true;
+  if (num < 1) {
+    console.log(`${num} is not prime.Provide positive integer`);
+  }
+  if (num === 1) {
+    isPrime = false;
+  }
+  if (num === 2) {
+    isPrime = true;
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    console.log(`${num} is a prime number `);
+  } else {
+    console.log(`${num} is not a prime number`);
+  }
+}
+primeNumber(6);
